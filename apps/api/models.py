f2c559e40d1617d3323e32fc0baa2040457ae2a1
Suresh Melvin Sigera <snowball@pop-os.localdomain>
2021-09-20 22:26:31 -0400

    astronaut model added

---
+++ apps/api/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.core.validators import MaxValueValidator
+
+
+# Create your models here.
+class Astronaut(models.Model):
+    first_name = models.CharField(max_length=255, null=False, blank=False)
+    last_name = models.CharField(max_length=255, null=False, blank=False)
+    percent_field = models.PositiveIntegerField(validators=[MaxValueValidator(100), ])
