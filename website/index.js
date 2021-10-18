82bf26835cce542ce463bf82639930fd8951bf35
shans96 <shanswanlow96@gmail.com>
2021-10-18 23:33:00 +0100

    Add session storage, fix bugs in page navigation after login

--- website/index.js
+++ website/index.js
@@ -10,23 +10,32 @@ $('#login').click(function() {
     };
 
     if (astronaut_selected) {
-        login_via_api("http://127.0.0.1:8000/auth/users/astronaut/login/", credentials);
+        login_via_api("http://127.0.0.1:8000/auth/users/astronaut/login/", credentials, "astronaut");
     } else if (surgeon_selected) {
-        login_via_api("http://127.0.0.1:8000/auth/users/scientist/login/", credentials);
+        login_via_api("http://127.0.0.1:8000/auth/users/scientist/login/", credentials, "scientist");
     } else {
         // Login can't proceed unless the user selects a role
         alert("Please select the role you are trying to sign in as.");
     }
 });
 
-function login_via_api(endpoint, credentials) {
+function login_via_api(endpoint, credentials, usertype) {
     $.ajax({
         url: endpoint,
         type: "POST",
         data: JSON.stringify(credentials),
         contentType:"application/json; charset=utf-8",
         success: function(data) {
-            // TODO: set JWT and redirect
+            // The token is stored using HTML5 session storage and JavaScript. This ensures that token storage is short-term only (Mozilla, 2021).
+            // XSS can be used to compromise the token if these technologies are used (OWASP, 2021).
+            // To minimize this risk, untrusted inputs are sanitized before rendering them- see in-space.js for details.
+            window.sessionStorage.setItem("token", data["token"]);
+            if (usertype == "astronaut") {
+                window.location.replace("http://localhost:8002/astronaut-details.html");
+            } else {
+                // If a scientist was authenticated, load this page
+                window.location.replace("http://localhost:8002/all-astronaut-records.html");
+            }
             console.log(data);
         },
         error: function(error) {
@@ -75,3 +84,4 @@ $("#surgeon-radio").click(function() {
 
 // References
 // Laverty, P. (2017) What is Username Enumeration? Available from: https://www.rapid7.com/blog/post/2017/06/15/about-user-enumeration/ [Accessed 14 October 2021].
+// OWASP. (2021) Cross Site Scripting (XSS). Available from: https://owasp.org/www-community/attacks/xss/ [Accessed 16 October 2021].
