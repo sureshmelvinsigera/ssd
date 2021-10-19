adc504b8350ca81fcc140d8c10312b5d3275fd5f
Suresh Melvin Sigera <snowball@pop-os.localdomain>
2021-10-19 17:38:46 -0400

    db seetings added

--- ssd/settings.py
+++ ssd/settings.py
@@ -79,10 +79,22 @@ WSGI_APPLICATION = 'ssd.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': "django.db.backends.postgresql",
+        'NAME': "ssd_iss",
+        'USER': "postgres",
+        'PASSWORD': "Xoco_137946",
+        'HOST': "3.129.7.239",
+        'PORT': "5432",
     }
 }
 
